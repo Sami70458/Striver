7e13b42fb478a5864979331475cefaa2619fc11b
Samiksha Bharti <115936670+Sami70458@users.noreply.github.com>
2026-03-13 20:51:39 +0530

    Update ValidAnagram.java

--- Strings/Easy/ValidAnagram.java
+++ Strings/Easy/ValidAnagram.java
@@ -31,3 +31,30 @@ public class ValidAnagram {
         System.out.println(ans);
     }
 }
+
+
+/*
+
+Time Complexity:
+
+Best Case: O(n)
+
+Average Case: O(n)
+
+Worst Case: O(n)
+
+(n = length of the strings s1 and s2)
+
+Reason:
+
+Counting characters in s1 → O(n)
+
+Updating counts using s2 → O(n)
+
+Final check of array of size 26 → O(26) ≈ O(1)
+
+Space Complexity:
+
+Auxiliary Space: O(1) (fixed-size array of 26 elements)
+
+                       */
